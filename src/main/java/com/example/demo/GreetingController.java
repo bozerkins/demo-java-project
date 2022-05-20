4bafaed9467f3d5a2091239b6b8fdc418dc73877
Bogdans Ozerkins <b.ozerkins@outlook.com>
2022-05-20 12:04:34 +0300

    initial commit

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(
+            @RequestParam(name="name", required=false, defaultValue="World") String name,
+            Model model
+    ) {
+        // write data to model
+        model.addAttribute("name", name);
+        // return name of the view
+        return "greeting";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        // return view name (hello.html)
+        return "hello";
+    }
+}
